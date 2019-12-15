ac04d85b9ba17e38f37e4760b433119654e5bfb0
galegoz <llucassduarte50@gmail.com>
2019-12-15 15:52:33 -0300

    Adição das colunas, icones, alinhamentos e campos

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,39 @@ class _homeState extends State<Home> {
   Widget build(BuildContext context) {
     // Scaffold representa o design com barras icones e etc.
     return Scaffold(
-        appBar: AppBar(
-      title: Text("Calculadora de IMC"),
-      centerTitle: true,
-      backgroundColor: Colors.deepPurple,
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: () {},
-        )
-      ],
-    ));
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        //alinhar eixo horizontal
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline, size: 120, color: Colors.green),
+          TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso em KG",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0)),
+          TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0)),
+        ],
+      ),
+    );
   }
 }
